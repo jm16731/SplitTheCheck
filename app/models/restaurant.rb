4a802274cb8d63bd5a18becb4b5778b488a7a800
jmurrel1 <jmurrel1@cs6242-vm15.uwg.westga.edu>
2022-03-03 11:08:37 -0500

    model validation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ApplicationRecord
+  validates :name, :location, :will_split, :wont_split, presence: true
+  validates :will_split, :wont_split, numericality: {
+    greater_than_or_equal_to: -1
+  }
 end
