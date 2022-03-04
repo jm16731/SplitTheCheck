1e51f75741ee8aef2a97e219053d646a4ddef646
jmurrel1 <jmurrel1@cs6242-vm15.uwg.westga.edu>
2022-03-04 15:42:14 -0500

    thumbs up/down tested

--- test/models/restaurant_test.rb
+++ test/models/restaurant_test.rb
@@ -11,11 +11,25 @@ class RestaurantTest < ActiveSupport::TestCase
 
   test "thumbs up" do
     @restaurant.thumbs_up
-    assert 8, @restaurant.will_split
+    assert_equal 8, @restaurant.will_split
+    assert_equal 3, @restaurant.wont_split
   end
 
   test "thumbs down" do
+    @restaurant.thumbs_down
+    assert_equal 7, @restaurant.will_split
+    assert_equal 4, @restaurant.wont_split
+  end
+
+  test "repeated thumbs up/down works" do
     @restaurant.thumbs_up
-    assert 6, @restaurant.will_split
+    @restaurant.thumbs_up
+    @restaurant.thumbs_up
+    @restaurant.thumbs_up
+    @restaurant.thumbs_down
+    @restaurant.thumbs_down
+    @restaurant.thumbs_down
+    assert_equal 11, @restaurant.will_split
+    assert_equal 6, @restaurant.wont_split
   end
 end
