ab017d0080b9a286c4131ef4fc40ec431736df92
jmurrel1 <jmurrel1@cs6242-vm15.uwg.westga.edu>
2022-03-04 15:21:53 -0500

    credits for scope logic

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,6 +4,7 @@ class Restaurant < ApplicationRecord
     greater_than_or_equal_to: -1
   }
 
+  #scope logic by https://www.justinweiss.com/articles/search-and-filter-rails-models-without-bloating-your-controller/
   scope :search_by_name,
     -> (name) { where("name like ?", "#{name}%") }
   scope :search_by_location,
