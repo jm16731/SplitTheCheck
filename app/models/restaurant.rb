c935ff8de9375a97458a859094396a4a87e950d1
jmurrel1 <jmurrel1@cs6242-vm15.uwg.westga.edu>
2022-03-04 15:38:30 -0500

    basic thumbs up/down function and test

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -9,4 +9,12 @@ class Restaurant < ApplicationRecord
     -> (name) { where("name like ?", "#{name}%") }
   scope :search_by_location,
     -> (location) { where("location like ?", "#{location}%") }
+
+  def thumbs_up
+    self.will_split = self.will_split + 1
+  end
+
+  def thumbs_down
+    self.wont_split = self.wont_split + 1
+  end
 end
--- test/models/restaurant_test.rb
+++ test/models/restaurant_test.rb
@@ -4,4 +4,18 @@ class RestaurantTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  setup do
+    @restaurant = restaurants(:one)
+  end
+
+  test "thumbs up" do
+    @restaurant.thumbs_up
+    assert 8, @restaurant.will_split
+  end
+
+  test "thumbs down" do
+    @restaurant.thumbs_up
+    assert 6, @restaurant.will_split
+  end
 end
