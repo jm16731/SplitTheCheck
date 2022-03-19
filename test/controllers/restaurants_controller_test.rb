a0a067cc67022d1c133ed0281ff387d7be9f982a
jmurrel1 <jmurrel1@cs6242-vm11.uwg.westga.edu>
2022-03-19 13:47:07 -0400

    controller tests expanded

--- test/controllers/restaurants_controller_test.rb
+++ test/controllers/restaurants_controller_test.rb
@@ -3,6 +3,8 @@ require "test_helper"
 class RestaurantsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @restaurant = restaurants(:one)
+    @thumbs_up_src = "/assets/thumbs_up-31deedd24365bb78a6a111e446096cedfc3d73bac7dce11130ec044cdf6c6880.png"
+    @thumbs_down_src = "/assets/thumbs_down-513a3b5d910cefc9dcc20a90ca33b4bc572f4b6041ee234ecfe41282a1416730.jpg"
   end
 
   teardown do
@@ -16,25 +18,48 @@ class RestaurantsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get restaurants_url
     assert_response :success
-    assert_select "h1", "Restaurants"
   end
 
-  test "should get first" do
+  test "index has header and table" do
+    get restaurants_url
+    assert_select "h1", "Restaurants"
+    assert_select "table thead tr th", count: 5
+    assert_select "table tbody tr", count: 10
+    assert_select "a", "Show", count: 10
+    assert_select "table tbody tr td", count: 7 * 10
+  end
+
+  test "index has search form" do
+    get restaurants_url
+    assert_select "input[type=submit]", 1
+    assert_select "a", "Clear"
+  end
+
+  test "index has navigation links" do
+    get restaurants_url
+    assert_select "a", "New Restaurant"
+    assert_select "a", "First"
+    assert_select "a", "Previous 10"
+    assert_select "a", "Next 10"
+    assert_select "a", "Last"
+  end
+
+  test "first_path should redirect to index" do
     get first_path
     assert_redirected_to restaurants_url
   end
 
-  test "should get last" do
+  test "last_path should redirect to index" do
     get last_path
     assert_redirected_to restaurants_url
   end
 
-  test "should get previous 10" do
+  test "previous_10_path should redirect to index" do
     get prev_10_path
     assert_redirected_to restaurants_url
   end
 
-  test "should get next 10" do
+  test "next_10_path should redirect to index" do
     get next_10_path
     assert_redirected_to restaurants_url
   end
@@ -43,47 +68,93 @@ class RestaurantsControllerTest < ActionDispatch::IntegrationTest
     get restaurants_url
     assert_nil session[:offset]
     get first_path
+    follow_redirect!
     assert_equal 0, session[:offset]
+    assert_select "table tbody tr", count: 10
   end
 
   test "going to last should set session to near max" do
     get restaurants_url
     assert_nil session[:offset]
     get last_path
+    follow_redirect!
     assert_equal 10, session[:offset]
+    assert_select "table tbody tr", count: 2
   end
 
   test "navigating back & forth works, doesn't overflow, can happen many times" do
     get restaurants_url
     assert_nil session[:offset]
+
     get last_path
+    follow_redirect!
     assert_equal 10, session[:offset]
+    assert_select "table tbody tr", count: 2
+
     get first_path
+    follow_redirect!
     assert_equal 0, session[:offset]
+    assert_select "table tbody tr", count: 10
+
     get first_path
+    follow_redirect!
     assert_equal 0, session[:offset]
+    assert_select "table tbody tr", count: 10
+
     get next_10_path
+    follow_redirect!
     assert_equal 10, session[:offset]
+    assert_select "table tbody tr", count: 2
+
     get next_10_path
+    follow_redirect!
     assert_equal 10, session[:offset]
+    assert_select "table tbody tr", count: 2
+
     get last_path
+    follow_redirect!
     assert_equal 10, session[:offset]
+    assert_select "table tbody tr", count: 2
+
     get last_path
+    follow_redirect!
     assert_equal 10, session[:offset]
+    assert_select "table tbody tr", count: 2
+
     get prev_10_path
+    follow_redirect!
     assert_equal 0, session[:offset]
+    assert_select "table tbody tr", count: 10
+
     get next_10_path
+    follow_redirect!
     assert_equal 10, session[:offset]
+    assert_select "table tbody tr", count: 2
+
     get prev_10_path
+    follow_redirect!
     assert_equal 0, session[:offset]
+    assert_select "table tbody tr", count: 10
+
     get prev_10_path
+    follow_redirect!
     assert_equal 0, session[:offset]
+    assert_select "table tbody tr", count: 10
+
     get first_path
+    follow_redirect!
     assert_equal 0, session[:offset]
+    assert_select "table tbody tr", count: 10
+
     get prev_10_path
+    follow_redirect!
     assert_equal 0, session[:offset]
+    assert_select "table tbody tr", count: 10
+
     get next_10_path
+    follow_redirect!
     assert_equal 10, session[:offset]
+    assert_select "table tbody tr", count: 2
   end
 
   test "should get new" do
@@ -92,7 +163,7 @@ class RestaurantsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create restaurant" do
-    assert_difference('Restaurant.count') do
+    assert_difference('Restaurant.count', 1) do
       post restaurants_url, params: {
         restaurant: {
           location: @restaurant.location,
@@ -108,8 +179,18 @@ class RestaurantsControllerTest < ActionDispatch::IntegrationTest
 
   test "should show restaurant" do
     get restaurant_url(@restaurant)
-    assert_select "h1", "Split the Check"
     assert_response :success
+  end
+
+  test "show page should have header, back link, thumbs up/down" do
+    get restaurant_url(@restaurant)
+    assert_select "h1", "Split the Check"
+    assert_select "a", "Back"
+    assert_select "img", count: 2
+    assert_select "img" do
+    	assert_select "[src=?]", @thumbs_up_src
+    	assert_select "[src=?]", @thumbs_down_src
+    end
   end
 
   test "should get edit" do
@@ -118,15 +199,25 @@ class RestaurantsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update restaurant" do
+    assert @restaurant.name = "Basil's"
+    assert @restaurant.location = "101 Johnny Mercer Blvd"
+    assert @restaurant.will_split = 7
+    assert @restaurant.wont_split = 3
+
     patch restaurant_url(@restaurant), params: {
       restaurant: {
-        location: @restaurant.location,
-        name: @restaurant.name,
-        will_split: @restaurant.will_split,
-        wont_split: @restaurant.wont_split
+        location: "112 Highway 80",
+        name: "Greg's Random Slop",
+        will_split: 12,
+        wont_split: 3
       }
     }
     assert_redirected_to restaurant_url(@restaurant)
+
+    assert @restaurant.name = "Greg's Random Slop"
+    assert @restaurant.location = "112 Highway 80"
+    assert @restaurant.will_split = 12
+    assert @restaurant.wont_split = 3
   end
 
   test "should destroy restaurant" do
@@ -139,17 +230,13 @@ class RestaurantsControllerTest < ActionDispatch::IntegrationTest
 
 =begin
   test "thumbs_up should redirect and increase" do
-    patch thumbs_up_path(@restaurant), params: {
-      id: 1
-    }
+    patch thumbs_up_path(@restaurant)
     assert_redirected_to restaurants_url
     assert_equal 8, @restaurant.will_split
   end
 
   test "thumbs_down should redirect and increase" do
-    patch thumbs_down_path(@restaurant),  params: {
-      id: 1
-    }
+    patch thumbs_down_path(@restaurant)
     assert_redirected_to restaurants_url
     assert_equal 4, @restaurant.wont_split
   end
