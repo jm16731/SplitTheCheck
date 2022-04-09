9c520d5605b05df9ad9ab4545b87fba9a500dbe2
jmurrel1 <jmurrel1@cs6242-vm11.uwg.westga.edu>
2022-04-09 11:10:30 -0400

    vote basic tests pass

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class VoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    #@restaurant = restaurants(:one)
+  end
+
+  test "vote attributes & references must not be empty" do
+    vote = Vote.new
+    assert vote.invalid?
+    assert vote.errors[:split].any?
+    assert_nil vote.restaurant_id
+    assert_nil vote.user_id
+    #assert vote.errors[:restaurant_id].any?
+    #assert vote.errors[:user_id].any?
+  end
 end
