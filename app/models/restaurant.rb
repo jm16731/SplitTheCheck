b8eb0f78860f35630613f92d4eef24d74e8070db
jmurrel1 <jmurrel1@cs6242-vm12.uwg.westga.edu>
2022-04-10 14:46:14 -0400

    code clean_up

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,20 +3,16 @@ class Restaurant < ApplicationRecord
   has_many :user, through: :vote
 
   validates :name, :location, presence: true
+  #validates :will_split, :wont_split, presence: true
+  #validates :will_split, :wont_split, numericality: {
+  #  greater_than_or_equal_to: 0
+  #}
 
   #scope logic by https://www.justinweiss.com/articles/search-and-filter-rails-models-without-bloating-your-controller/
   scope :search_by_name,
     -> (name) { where("name like ?", "#{name}%") }
   scope :search_by_location,
     -> (location) { where("location like ?", "#{location}%") }
-
-
-  #Leftover will_split and wont_split from previous version
-
-  #validates :will_split, :wont_split, presence: true
-  #validates :will_split, :wont_split, numericality: {
-  #  greater_than_or_equal_to: 0
-  #}
 
   def thumbs_up(user)
     #update_attribute(:will_split, will_split + 1)
