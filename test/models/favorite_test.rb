8f882d24ab9e123573e9d809e16a0f30a5b1a56b
jmurrel1 <jmurrel1@cs6242-vm07.uwg.westga.edu>
2022-04-30 11:41:13 -0400

    favorite model tests pass

--- test/models/favorite_test.rb
+++ test/models/favorite_test.rb
@@ -1,7 +1,46 @@
 require "test_helper"
 
 class FavoriteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @favorite = favorites(:one)
+  end
+
+  test "favorites references must not be empty" do
+    favorite = Favorite.new
+    assert favorite.invalid?
+    assert_nil favorite.restaurant_id
+    assert_nil favorite.user_id
+    assert_equal 2, Favorite.count
+  end
+
+  test "can create new favorite by new" do
+    favorite = Favorite.new(
+      restaurant: restaurants(:joe),
+      user: users(:admin)
+    )
+    assert favorite.valid?
+    assert favorite.restaurant_id.present?
+    assert favorite.user_id.present?
+
+    favorite.save
+    assert_equal 3, Favorite.count
+  end
+
+  test "fvaorite valid if have references, done by create" do
+    favorite = Favorite.create!(
+      restaurant: restaurants(:joe),
+      user: users(:admin)
+    )
+    assert favorite.valid?
+
+    assert_equal 3, Favorite.count
+
+    favorite = Favorite.create!(
+      restaurant: restaurants(:wiley),
+      user: users(:dork)
+    )
+    assert favorite.valid?
+
+    assert_equal 4, Favorite.count
+  end
 end
